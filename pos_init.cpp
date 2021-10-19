ea25a0695a5138d164b6070365ecb8bed8b507f6
aisse-258 <y-kate@ukr.net>
2021-10-19 23:53:13 +0300

    Генерация позиций класса Position

--- pos_init.cpp
+++ pos_init.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <QString>
 #include <QTextStream>
+#include "position_class.h"
 using namespace std;
-void pos_init(QString ***positions, int chessBoardSize) {//генерация таблицы позиций
+void pos_init(Position **positions, int chessBoardSize) {//генерация таблицы позиций
 	for (int i=0;i<chessBoardSize;i++){
 		int c='a';
 		for (int j=0;j<chessBoardSize;j++){
-			positions[i][j][0] = c+QString::number(i+1);
-			positions[i][j][1] = QString::number(14 + j*30);
-			positions[i][j][2] = QString::number(223 - i*30);
+			positions[i][j].pos_name = c+QString::number(i+1);
+			positions[i][j].x = 14 + j*30;
+			positions[i][j].y = 223 - i*30;
 			//out<<positions[i][j][0]<<"\t"<<positions[i][j][1]<<"\t"<<positions[i][j][2]<<"\t";
 			c++;
 		}
--- pos_init.h
+++ pos_init.h
@@ -3,9 +3,10 @@
 
 #include <iostream>
 #include <QString>
+#include "position_class.h"
 using namespace std;
 
-void pos_init(QString ***positions, int chessBoardSize = 8);
+void pos_init(Position **positions, int chessBoardSize = 8);
 
 #endif // POS_INIT
 
