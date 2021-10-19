1fbec7a3003f7911fcf7d17bf3b153b771745abd
aisse-258 <y-kate@ukr.net>
2021-10-19 11:54:10 +0300

    Генерация позиций вместе с координатами на доске

--- knight.cpp
+++ knight.cpp
@@ -5,7 +5,7 @@ using namespace std;
 QTextStream out(stdout);
 const int chessBoardSize = 8;
 int chessBoard [chessBoardSize][chessBoardSize] = {0};
-const QString positions [chessBoardSize][chessBoardSize] = {
+QString positions [chessBoardSize][chessBoardSize][3]/* = {
 	{"a1","b1","c1","d1","e1","f1","g1","h1"},
 	{"a2","b2","c2","d2","e2","f2","g2","h2"},
 	{"a3","b3","c3","d3","e3","f3","g3","h3"},
@@ -14,22 +14,55 @@ const QString positions [chessBoardSize][chessBoardSize] = {
 	{"a6","b6","c6","d6","e6","f6","g6","h6"},
 	{"a7","b7","c7","d7","e7","f7","g7","h7"},
 	{"a8","b8","c8","d8","e8","f8","g8","h8"}
-};
-QString *stepPositions;
+}*/;
+void pos_init() {//генерация таблицы позиций
+	for (int i=0;i<chessBoardSize;i++){
+		int c='a';
+		for (int j=0;j<chessBoardSize;j++){
+			positions[i][j][0] = c+QString::number(i+1);
+			positions[i][j][1] = QString::number(14 + j*30);
+			positions[i][j][2] = QString::number(223 - i*30);
+			//out<<positions[i][j][0]<<"\t"<<positions[i][j][1]<<"\t"<<positions[i][j][2]<<"\t";
+			c++;
+		}
+		//out<<endl;
+	}
+}
+QString **stepPositions;//здесь будет путь
 int knight (QString pos1, QString pos2) {
+	pos_init();
 	int step = 0;
-	if (QString::compare(pos1, pos2) == 0) return step;
 	int i,j;
-	int pos_i, pos_j;
+	if (QString::compare(pos1, pos2) == 0) {//если позиции равны
+		stepPositions = new QString *[step+1];//путь состоит из одной позиции
+		stepPositions[0] = new QString [3];
+		stepPositions[0][0] = pos1;
+		for (i=0;i<chessBoardSize;i++){//всё равно сохраняем координаты первой позиции, чтобы отобразить там коня
+			for (j=0; j<chessBoardSize; j++){
+				if (QString::compare(pos1, positions[i][j][0]) == 0) {
+					stepPositions[0][1] = positions[i][j][1];
+					stepPositions[0][2] = positions[i][j][2];
+					chessBoard[i][j] = 9999;
+				}
+				else {
+					chessBoard[i][j] = -1;
+				}
+			}
+		}
+		return step;
+	}
+	int pos_i1, pos_j1, pos_i2, pos_j2;
 	for (i=0;i<chessBoardSize;i++){//отметили начальную и конечную позиции на доске
 		for (j=0; j<chessBoardSize; j++){
-			if (QString::compare(pos1, positions[i][j]) == 0) {
+			if (QString::compare(pos1, positions[i][j][0]) == 0) {
+				pos_i1 = i;
+				pos_j1 = j;
 				chessBoard[i][j] = 0;
 			}
-			else if (QString::compare(pos2, positions[i][j]) == 0) {
+			else if (QString::compare(pos2, positions[i][j][0]) == 0) {
 				chessBoard[i][j] = 9999;
-				pos_i = i;//запоминаем, где конец, чтобы строить то него путь обратно
-				pos_j = j;
+				pos_i2 = i;//запоминаем, где конец, чтобы строить то него путь обратно
+				pos_j2 = j;
 			}
 			else {
 				chessBoard[i][j] = -1;
@@ -131,132 +164,155 @@ int knight (QString pos1, QString pos2) {
 		}
 		out<<endl;
 	}
-	stepPositions = new QString[step + 1];
-	stepPositions[0] = pos1;
-	stepPositions[step] = pos2;
+	stepPositions = new QString*[step + 1];
+	for(i=0;i<step + 1;i++){
+		stepPositions[i] = new QString[3];
+	}
+	stepPositions[0][0] = pos1;//сохраняем начало и конец пути
+	stepPositions[0][1] = positions[pos_i1][pos_j1][1];
+	stepPositions[0][2] = positions[pos_i1][pos_j1][2];
+	stepPositions[step][0] = pos2;
+	stepPositions[step][1] = positions[pos_i2][pos_j2][1];
+	stepPositions[step][2] = positions[pos_i2][pos_j2][2];
 	if (step == 1) {
 		return step;
 	}
 	int stepBack = step;
 	do{
 		stepBack--;
-		if(pos_i+1<8 && pos_j+2<8){//ищем возможный предыдущий шаг
-			if (chessBoard[pos_i+1][pos_j+2] == 0) {
+		if(pos_i2+1<8 && pos_j2+2<8){//ищем возможный предыдущий шаг
+			if (chessBoard[pos_i2+1][pos_j2+2] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i+1][pos_j+2] == stepBack){
-				stepPositions[stepBack] = positions[pos_i+1][pos_j+2];//если нашли, помещаем его в список шагов
-				pos_i = pos_i+1;//следующий будем искать с этой точки
-				pos_j = pos_j+2;
+			else if (chessBoard[pos_i2+1][pos_j2+2] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2+1][pos_j2+2][0];//если нашли, помещаем его в список шагов
+				stepPositions[stepBack][1] = positions[pos_i2+1][pos_j2+2][1];
+				stepPositions[stepBack][2] = positions[pos_i2+1][pos_j2+2][2];
+				pos_i2 = pos_i2+1;//следующий будем искать с этой точки
+				pos_j2 = pos_j2+2;
 				continue;
 			}
 		}
-		if(pos_i-1>=0 && pos_j+2<8){
-			if (chessBoard[pos_i-1][pos_j+2] == 0) {
+		if(pos_i2-1>=0 && pos_j2+2<8){
+			if (chessBoard[pos_i2-1][pos_j2+2] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i-1][pos_j+2] == stepBack){
-				stepPositions[stepBack] = positions[pos_i-1][pos_j+2];
-				pos_i = pos_i-1;
-				pos_j = pos_j+2;
+			else if (chessBoard[pos_i2-1][pos_j2+2] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2-1][pos_j2+2][0];
+				stepPositions[stepBack][1] = positions[pos_i2-1][pos_j2+2][1];
+				stepPositions[stepBack][2] = positions[pos_i2-1][pos_j2+2][2];
+				pos_i2 = pos_i2-1;
+				pos_j2 = pos_j2+2;
 				continue;
 			}
 		}
-		if(pos_i-2>=0 && pos_j+1<8){
-			if (chessBoard[pos_i-2][pos_j+1] == 0) {
+		if(pos_i2-2>=0 && pos_j2+1<8){
+			if (chessBoard[pos_i2-2][pos_j2+1] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i-2][pos_j+1] == stepBack){
-				stepPositions[stepBack] = positions[pos_i-2][pos_j+1];
-				pos_i = pos_i-2;
-				pos_j = pos_j+1;
+			else if (chessBoard[pos_i2-2][pos_j2+1] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2-2][pos_j2+1][0];
+				stepPositions[stepBack][1] = positions[pos_i2-2][pos_j2+1][1];
+				stepPositions[stepBack][2] = positions[pos_i2-2][pos_j2+1][2];
+				pos_i2 = pos_i2-2;
+				pos_j2 = pos_j2+1;
 				continue;
 			}
 		}
-		if(pos_i-2>=0 && pos_j-1>=0){
-			if (chessBoard[pos_i-2][pos_j-1] == 0) {
+		if(pos_i2-2>=0 && pos_j2-1>=0){
+			if (chessBoard[pos_i2-2][pos_j2-1] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i-2][pos_j-1] == stepBack){
-				stepPositions[stepBack] = positions[pos_i-2][pos_j-1];
-				pos_i = pos_i-2;
-				pos_j = pos_j-1;
+			else if (chessBoard[pos_i2-2][pos_j2-1] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2-2][pos_j2-1][0];
+				stepPositions[stepBack][1] = positions[pos_i2-2][pos_j2-1][1];
+				stepPositions[stepBack][2] = positions[pos_i2-2][pos_j2-1][2];
+				pos_i2 = pos_i2-2;
+				pos_j2 = pos_j2-1;
 				continue;
 			}
 		}
-		if(i-1>=0 && pos_j-2>=0){
-			if (chessBoard[pos_i-1][pos_j-2] == 0) {
+		if(i-1>=0 && pos_j2-2>=0){
+			if (chessBoard[pos_i2-1][pos_j2-2] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i-1][pos_j-2] == stepBack){
-				stepPositions[stepBack] = positions[pos_i-1][pos_j-2];
-				pos_i = pos_i-1;
-				pos_j = pos_j-2;
+			else if (chessBoard[pos_i2-1][pos_j2-2] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2-1][pos_j2-2][0];
+				stepPositions[stepBack][1] = positions[pos_i2-1][pos_j2-2][1];
+				stepPositions[stepBack][2] = positions[pos_i2-1][pos_j2-2][2];
+				pos_i2 = pos_i2-1;
+				pos_j2 = pos_j2-2;
 				continue;
 			}
 		}
-		if(pos_i+1<8 && pos_j-2>=0){
-			if (chessBoard[pos_i+1][pos_j-2] == 0) {
+		if(pos_i2+1<8 && pos_j2-2>=0){
+			if (chessBoard[pos_i2+1][pos_j2-2] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i+1][pos_j-2] == stepBack){
-				stepPositions[stepBack] = positions[pos_i+1][pos_j-2];
-				pos_i = pos_i+1;
-				pos_j = pos_j-2;
+			else if (chessBoard[pos_i2+1][pos_j2-2] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2+1][pos_j2-2][0];
+				stepPositions[stepBack][1] = positions[pos_i2+1][pos_j2-2][1];
+				stepPositions[stepBack][2] = positions[pos_i2+1][pos_j2-2][2];
+				pos_i2 = pos_i2+1;
+				pos_j2 = pos_j2-2;
 				continue;
 			}
 		}
-		if(pos_i+2<8 && pos_j-1>=0){
-			if (chessBoard[pos_i+2][pos_j-1] == 0) {
+		if(pos_i2+2<8 && pos_j2-1>=0){
+			if (chessBoard[pos_i2+2][pos_j2-1] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i+2][pos_j-1] == stepBack){
-				stepPositions[stepBack] = positions[pos_i+2][pos_j-1];
-				pos_i = pos_i+2;
-				pos_j = pos_j-1;
+			else if (chessBoard[pos_i2+2][pos_j2-1] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2+2][pos_j2-1][0];
+				stepPositions[stepBack][1] = positions[pos_i2+2][pos_j2-1][1];
+				stepPositions[stepBack][2] = positions[pos_i2+2][pos_j2-1][2];
+				pos_i2 = pos_i2+2;
+				pos_j2 = pos_j2-1;
 				continue;
 			}
 		}
-		if(pos_i+2<8 && pos_j+1<8){
-			if (chessBoard[pos_i+2][pos_j+1] == 0) {
+		if(pos_i2+2<8 && pos_j2+1<8){
+			if (chessBoard[pos_i2+2][pos_j2+1] == 0) {
 				for(i=0;i<=step;i++){
-					out<<stepPositions[i]<<"\t";
+					out<<stepPositions[i][0]<<"\t";
 				}
 				out<<endl;
 				return step;
 			}
-			else if (chessBoard[pos_i+2][pos_j+1] == stepBack){
-				stepPositions[stepBack] = positions[pos_i+2][pos_j+1];
-				pos_i = pos_i+2;
-				pos_j = pos_j+1;
+			else if (chessBoard[pos_i2+2][pos_j2+1] == stepBack){
+				stepPositions[stepBack][0] = positions[pos_i2+2][pos_j2+1][0];
+				stepPositions[stepBack][1] = positions[pos_i2+2][pos_j2+1][1];
+				stepPositions[stepBack][2] = positions[pos_i2+2][pos_j2+1][2];
+				pos_i2 = pos_i2+2;
+				pos_j2 = pos_j2+1;
 				continue;
 			}
 		}
